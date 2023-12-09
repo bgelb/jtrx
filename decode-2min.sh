5b4a7797feb1f76b52bdb20c16028fcb9af23529
Ben Gelb <ben.gelb@gmail.com>
2023-12-09 03:26:28 +0000

    run gain/phase search in decode loop

--- decode-2min.sh
+++ decode-2min.sh
@@ -27,11 +27,7 @@ while [ 1 ] ; do
 	cat ../$DATADIR/$PERIOD_PREV_2.dat ../$DATADIR/$PERIOD_PREV_1.dat > 2min_temp.dat
 	/usr/bin/python ../towav.py --infile 2min_temp.dat --outfile $PERIOD_PREV_2.wav
 
-	# magic phase/gain values
-	/usr/bin/python ../phase.py --infile 2min_temp.dat --phi 245.0 --gain 4.85
-	mv combined_out.wav ch3_${PERIOD_PREV_2}.wav
-
-	for i in {1..3}
+	for i in {1..2}
 	do
 		echo "`date`: Processing Channel $i" >> decode.log
 		/usr/bin/wsprd -d -f 0.4742 ch${i}_${PERIOD_PREV_2}.wav >> decode.log
@@ -56,6 +52,39 @@ while [ 1 ] ; do
 		        echo "`date`: curl result: $RESULT , done." >> decode.log
 		fi
 	done
+
+	echo "running gain/phase search" >> decode.log
+	PHASE_ARGS=`/usr/bin/python ../scripts/search.py --infile 2min_temp.dat | tee -a decode.log | head -n1 | sed 's/^.*gain=\([0-9]*\), phi=\([0-9]*\).*$/--gain \1 --phi \2/'`
+	echo "phase.py $PHASE_ARGS" >> decode.log
+	/usr/bin/python ../phase.py --infile 2min_temp.dat $PHASE_ARGS
+	mv combined_out.wav ch3_${PERIOD_PREV_2}.wav
+
+	for i in {3..3}
+	do
+		echo "`date`: Processing Channel $i" >> decode.log
+		/usr/bin/wsprd -d -f 0.4742 ch${i}_${PERIOD_PREV_2}.wav >> decode.log
+		FILESIZE=$(stat -c%s "wspr_spots.txt")
+		if [ $FILESIZE -ne 0 ] ; then
+
+		        # add the spots to a temporary file used for uploading to wsprnet.org
+		        cat wspr_spots.txt >> wsprdsum_ch$i.out
+
+		        # upload the spots
+		        echo "`date`: upload by curl" >> decode.log
+		        /usr/bin/curl -m 10 -F allmept=@wsprdsum_ch$i.out -F call=${MYCALL}${i} -F grid=${MYGRID} http://wsprnet.org/meptspots.php >> decode.log;
+		        RESULT=$?
+
+		        # check if curl uploaded the data successfully
+		        # delete only if uploaded
+		        if [ $RESULT -eq 0 ] ; then
+		                # data uploaded, delete them
+		                echo "`date`: Upload OK, deleting" >> decode.log
+		                rm wsprdsum_ch$i.out
+		        fi
+		        echo "`date`: curl result: $RESULT , done." >> decode.log
+		fi
+	done
+
 
 	rm ch1_$PERIOD_PREV_2.wav
 	rm ch2_$PERIOD_PREV_2.wav
